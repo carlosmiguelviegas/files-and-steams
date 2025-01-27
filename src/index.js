4b932c5143c833343cfda8ed5d02d7edd46fb7a0
unknown <carlosmviegas86@gmail.com>
2025-01-27 22:16:00 +0000

    read the content of the file assynchrounously

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ const fs = require('fs');
 fs.writeFile('./test.txt', 'just a test', (err) => {
   err ? console.log(err) : console.log('Content successfully written.');
 });
+
+fs.readFile('./test.txt', 'utf-8', (err, content) => {
+  err ? console.log(err) : console.log(content);
+});
