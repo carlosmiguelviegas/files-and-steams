4729680c086c0f71bddd1c9d021072f32d0a607b
unknown <carlosmviegas86@gmail.com>
2025-01-28 18:58:53 +0000

    created a read stream and a write stream. piped them to write a second file with the content of the first one

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 
-fs.writeFile('./test.txt', 'just a test', (err) => {
+fs.writeFile('./test.txt', 'just a test\nanother line', (err) => {
   err ? console.log(err) : console.log('Content successfully written.');
 });
 
 fs.readFile('./test.txt', 'utf-8', (err, content) => {
   err ? console.log(err) : console.log(content);
 });
+
+const readStream = fs.createReadStream('./test.txt', 'utf-8');
+const writeStream = fs.createWriteStream('./test2.txt');
+
+readStream.pipe(writeStream);
