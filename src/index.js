d6d606ad1468bba36960130f2af72c6d731d0eec
unknown <carlosmviegas86@gmail.com>
2025-01-29 23:06:04 +0000

    setup express server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+const express = require('express');
 const fs = require('fs');
+
+const PORT = 8000;
+
+const app = express();
+app.use(express.json());
 
 fs.writeFile('./test.txt', 'just a test\nanother line', (err) => {
   err ? console.log(err) : console.log('Content successfully written.');
@@ -12,3 +18,10 @@ const readStream = fs.createReadStream('./test.txt', 'utf-8');
 const writeStream = fs.createWriteStream('./test2.txt');
 
 readStream.pipe(writeStream);
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+});
+
+app.listen(PORT, () => console.log('server connected and listening to requests...'));
