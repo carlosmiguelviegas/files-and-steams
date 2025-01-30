2d348234346a9f47929557d61bddae872ad56f46
unknown <carlosmviegas86@gmail.com>
2025-01-30 13:56:03 +0000

    installed multer and configured it

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
 const parser = require('csv-parser');
+const multer = require('multer');
 
 const PORT = 8000;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const upload = multer({ dest: 'files' });
 
 /* fs.writeFile('./test.txt', 'just a test\nanother line', (err) => {
   err ? console.log(err) : console.log('Content successfully written.');
@@ -22,10 +25,9 @@ const writeStream = fs.createWriteStream('./test2.txt');
 
 readStream.pipe(writeStream); */
 
-app.post('/', (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
-  fs.createReadStream(req.file.path).pipe(parser()).on('data', content => console.log(content));
+app.post('/', upload.single('file'), (req, res) => {
+  console.log(req.params.file);
+  fs.createReadStream(req.file).pipe(parser()).on('data', content => console.log(content));
 });
 
 app.listen(PORT, () => console.log('server connected and listening to requests...'));
