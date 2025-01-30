36e2fc1173d09bfe16f4a0dd18293e2feb1e7abd
unknown <carlosmviegas86@gmail.com>
2025-01-30 19:27:00 +0000

    restored

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-const express = require('express');
 const fs = require('fs');
-const cors = require('cors');
-const parser = require('csv-parser');
-const multer = require('multer');
-
-const PORT = 8000;
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-const upload = multer({ dest: 'files' });
 
 fs.writeFile('./test.txt', 'just a test\nanother line', (err) => {
   err ? console.log(err) : console.log('Content successfully written.');
@@ -24,22 +12,3 @@ const readStream = fs.createReadStream('./test.txt', 'utf-8');
 const writeStream = fs.createWriteStream('./test2.txt');
 
 readStream.pipe(writeStream);
-
-app.post('/', upload.single('file'), (req, res) => {
-
-  const { path } = req['file']; 
-  const rows = [];
-  
-  fs.createReadStream(path)
-  .pipe(parser())
-  .on('data', chunck => rows.push(chunck))
-  .on('end', () => {
-    fs.unlink(path, (err) => { if (err) console.log(err) });
-    res.status(200).json({ message: `File processed with success. It were processed ${rows.length} records.` })
-  })
-  .on('error', (err) => {
-    if (err) res.status(200).json({ message: `File processed with success. It were processed ${rows.length} records.` })
-  });
-});
-
-app.listen(PORT, () => console.log('server connected and listening to requests...'));
