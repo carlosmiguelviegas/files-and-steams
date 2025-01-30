06e8db7b8b3ca7b7658c478fb1baf17024e69d12
unknown <carlosmviegas86@gmail.com>
2025-01-30 13:05:18 +0000

    added code to create a read stream and add the parser to convert the csv file into json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const fs = require('fs');
+const parser = require('csv-parser');
 
 const PORT = 8000;
 
 const app = express();
 app.use(express.json());
 
-fs.writeFile('./test.txt', 'just a test\nanother line', (err) => {
+/* fs.writeFile('./test.txt', 'just a test\nanother line', (err) => {
   err ? console.log(err) : console.log('Content successfully written.');
 });
 
@@ -17,11 +18,12 @@ fs.readFile('./test.txt', 'utf-8', (err, content) => {
 const readStream = fs.createReadStream('./test.txt', 'utf-8');
 const writeStream = fs.createWriteStream('./test2.txt');
 
-readStream.pipe(writeStream);
+readStream.pipe(writeStream); */
 
 app.post('/', (req, res) => {
   console.log(req.body);
   console.log(req.file);
+  fs.createReadStream(req.file.path).pipe(parser()).on('data', content => console.log(content));
 });
 
 app.listen(PORT, () => console.log('server connected and listening to requests...'));
